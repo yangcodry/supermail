cab17418ce048962fbd70ecbcef2775c16a0d6d6
yangzhi <2422586279@qq.com>
2021-03-02 08:42:35 +0800

    'tabbar初步'

---
+++ src/components/context/hometabbar/homeTabbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="tabbar">
+    <div class="tabbar-item"></div>
+    <div class="tabbar-item"></div>
+    <div class="tabbar-item"></div>
+    <div class="tabbar-item"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "homeTabbar"
+  }
+</script>
+
+<style scoped>
+  .tabbar {
+    width: 49px;
+    display: flex;
+    margin-left: 0;
+    margin-right: 0;
+    margin-bottom: 0;
+    position:fixed;
+  }
+  .tabbar-item {
+    background: yellow;
+    flex: 1;
+  }
+</style>
---
+++ src/test.js
@@ -0,0 +1,15 @@
+/*
+module.exports = {
+  a: 3,
+  b: 4
+}
+var {a,b} = require('test')
+export {
+
+}
+import {a,b} from 'test'
+export default a
+import a from ''*/
+
+let arr = ' '
+console.log(arr.split('_').length)
